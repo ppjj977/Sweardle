4498896a294d5b9c38b6b7e75b886482d78107cb
ppjj977 <98964607+ppjj977@users.noreply.github.com>
2022-02-04 21:55:56 +0000

    Update sweardle.js

--- sweardle.js
+++ sweardle.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     createSquares();
     addKeyboardClicks();
     loadLocalStorage();
+    resetGameState();
 
     function resetGameState() {
         window.localStorage.removeItem("guessedWordCount");
