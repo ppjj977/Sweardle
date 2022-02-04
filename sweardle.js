8ea968b6a6d7776aa8f833aa1c055b7e5878cfe4
ppjj977 <98964607+ppjj977@users.noreply.github.com>
2022-02-04 14:52:31 +0000

    Update sweardle.js

--- sweardle.js
+++ sweardle.js
@@ -213,21 +213,7 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    try {
-      const res = await fetch(
-        `https://wordsapiv1.p.rapidapi.com/words/${guessedWord.toLowerCase()}`,
-        {
-          method: "GET",
-          headers: {
-            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-            "x-rapidapi-key": "<YOUR_KEY_HERE>",
-          },
-        }
-      );
-
-      if (!res.ok) {
-        throw Error();
-      }
+   
       const firstLetterId = guessedWordCount * 5 + 1;
 
       localStorage.setItem("availableSpace", availableSpace);
